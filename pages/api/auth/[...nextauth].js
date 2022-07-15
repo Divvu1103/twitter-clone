d756747842f5ba6482512600bafc4f1e3158248a
Divvu1103 <200110075@hbtu.ac.in>
2022-07-15 13:16:20 +0530

    hosted

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth'
-import TwitterProvider from 'next-auth/providers/twitter'
+
 import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
   // Configure one or more authentication providers
